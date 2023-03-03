ff5dfe550d4668f2a539fb563e0feff498d87ec9
Adham Al Koukou <adhamalkoukou@gmail.com>
2023-03-03 12:16:33 -0500

    made the model and routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_03_162407) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_03_170517) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,6 +77,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_162407) do
     t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "comment"
+    t.integer "rating"
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -96,4 +105,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_162407) do
   add_foreign_key "bookings", "dolls"
   add_foreign_key "bookings", "users"
   add_foreign_key "dolls", "users"
+  add_foreign_key "reviews", "bookings"
 end
